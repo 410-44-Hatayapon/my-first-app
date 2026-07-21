c3de43feaa5345dc9d23465b9fbe8bd76adbd73f
410-44-Hatayapon <56822@yupparaj.ac.th>
2026-07-21 13:34:14 +0700

    Create convertYear.py

---
+++ convertYear.py
@@ -0,0 +1,6 @@
+import stemlit as st
+st.title("แอปพลิเคชั่นแปลงปี พ.ศ. เป็น ค.ศ. ")
+
+bh_year=st.number_input("แอปพลิเคชั่นแปลงปี พ.ศ. เป็น ค.ศ. ",value=2569)
+ce_year=bh_year-543
+st.header(f"ปี ค.ศ. คือ : {ce_year}")
