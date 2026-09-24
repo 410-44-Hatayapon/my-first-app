e4b5e21b920305632265b9a72c1fa0d17c675bc3
410-44-Hatayapon <56822@yupparaj.ac.th>
2026-09-24 10:12:38 +0700

    Update fruity_shop.app.py

--- fruity_shop.app.py
+++ fruity_shop.app.py
@@ -53,8 +53,8 @@ fruits = {
 # =========================
 # สร้างตัวแปรจำนวนสินค้า
 # สร้างตัวแปรจำนวนสินค้า
- for fruit in fruits:
-     if f"qty_{fruit}" not in st.session_state:
+for fruit in fruits:
+    if f"qty_{fruit}" not in st.session_state:
         st.session_state[f"qty_{fruit}"] = 0
 
 # =========================
